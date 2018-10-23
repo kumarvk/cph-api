b8309e5f4f81577186c16a705fbfff2781688cd7
Vkumar <kumarvk.pool@gmail.com>
2018-10-23 07:00:21 +0530

    Install dependecy.

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module CphApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.logger = Logger.new(STDOUT)
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
