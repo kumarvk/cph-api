14d80b4a4cf2ccbf25c47718ab09886a59fe5f47
Vkumar <kumarvk.pool@gmail.com>
2018-10-25 08:26:03 +0530

    Fixed factorybot DEPRECATION WARNING issues.

--- spec/factories/flight.rb
+++ spec/factories/flight.rb
@@ -1,19 +1,21 @@
 require 'faker'
 
 FactoryBot.define do
+  sequence(:type) {|n| "Arrival" }
   sequence(:exact_time) {|n| Faker::Time.unique.between(DateTime.now.beginning_of_day, DateTime.now.end_of_day) }
   sequence(:destination) {|n| Faker::Address.full_address }
   sequence(:flight_no) {|n| Faker::Code.asin }
   sequence(:airline) {|n| Faker::Company.industry }
+  sequence(:status) {|n| Faker::String.random(4) }
+  sequence(:date) {|n| Time.now }
 
   factory :flight do
-    type "Arrival"
-    date Time.now
+    type { FactoryBot.generate(:type) }
+    date { FactoryBot.generate(:date) }
     exact_time { FactoryBot.generate(:exact_time) }
-    expected_time nil
     destination { FactoryBot.generate(:destination) }
     flight_no { FactoryBot.generate(:flight_no) }
     airline { FactoryBot.generate(:airline) }
-    status "Closed"
+    status { FactoryBot.generate(:status) }
   end
 end
